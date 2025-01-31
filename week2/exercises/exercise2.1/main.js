8743c0f2adda4b1b0bcdeee645b2a2d3687f0f7c
freidamy <freidamy@gmail.com>
2025-01-30 21:22:38 -0500

    week 2 exercise 2.1

---
+++ week2/exercises/exercise2.1/main.js
@@ -0,0 +1,11 @@
+// favorite movies
+let movieList = ["The Matrix", "Inception", "Avatar"];
+
+// prompt
+let newMovie = prompt("What is your favorite movie?");
+
+// add new movie
+movieList.push(newMovie);
+
+// show full list
+alert("All movies: " + movieList.toString());
