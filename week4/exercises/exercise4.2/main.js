8d8a9107d205101b41d92a6241af9ea829678e22
freidamy <freidamy@gmail.com>
2025-02-13 23:01:30 -0500

    week 4 exercise 2

---
+++ week4/exercises/exercise4.2/main.js
@@ -0,0 +1,34 @@
+let rectX = 0;
+const rectWidth = 75;
+let clickCount = 0;
+
+function setup() {
+	createCanvas(500, 500);
+}
+
+function draw(){
+	background(155);
+	drawShape();
+	rectX ++;
+
+	if(rectX > width){
+		noLoop();
+		text('Your score was ' + clickCount, 100, 300);
+	}
+}
+
+function mousePressed(){
+	if((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= 0 && mouseY <= 75)) {
+		
+		clickCount++;
+		console.log('hit', clickCount);
+	}
+
+}
+
+
+
+function drawShape(){
+	fill('purple');
+	rect(rectX,0,rectWidth,75);
+}
