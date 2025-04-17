4de78f8e14a3443db92534b128381b51a74f75d9
freidamy <freidamy@gmail.com>
2025-04-16 20:09:21 -0400

    added animation

--- week11/exercise11.2/sketch.js
+++ week11/exercise11.2/sketch.js
@@ -1,16 +1,21 @@
+let spacing = 40;
+
 function setup() {
   createCanvas(600, 600);
-  background(245);
-  stroke(30);
   noFill();
+  stroke(30);
+}
 
-  let spacing = 40;
+function draw() {
+  background(245);
+  let time = millis() * 0.002; // smooth time-based animation
 
   for (let x = spacing; x < width; x += spacing) {
     for (let y = spacing; y < height; y += spacing) {
       let d = dist(x, y, width / 2, height / 2);
-      let size = map(d, 0, width / 2, 20, 2);
-      ellipse(x, y, size, size);
+      let baseSize = map(d, 0, width / 2, 5, 20);
+      let animatedSize = baseSize + sin(time + d * 0.05) * 4;
+      ellipse(x, y, animatedSize);
     }
   }
 }
